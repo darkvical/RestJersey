f15fe4b01a4fa3cea340c92612365d3d6f64b9c7
Victor Rodriguez <vical.rl@gmail.com>
2020-11-01 16:20:15 -0300

    extra

--- src/main/java/com/vical/rest/IPICServiceRest.java
+++ src/main/java/com/vical/rest/IPICServiceRest.java
@@ -1,12 +1,17 @@
 package com.vical.rest;
 
+import java.util.List;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import com.vical.domain.Persona;
+import com.vical.domain.User;
 
 
 @Path("/ejemplo")
@@ -23,5 +28,11 @@ public interface IPICServiceRest {
 	@Path("/verificar")
 	@Produces(MediaType.APPLICATION_JSON)
 	public String verificarService(@QueryParam("argument") String argumento);
+	
+	@POST
+	@Path("/crearUser")
+	@Produces({MediaType.APPLICATION_JSON})
+	@Consumes({MediaType.APPLICATION_JSON})
+	public void verificarService(List<User> users);
 	
 }
--- src/main/java/com/vical/rest/impl/PICServiceRestImpl.java
+++ src/main/java/com/vical/rest/impl/PICServiceRestImpl.java
@@ -1,18 +1,31 @@
 package com.vical.rest.impl;
 
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
+import com.google.gson.Gson;
+import com.pusher.rest.Pusher;
 import com.vical.domain.Persona;
+import com.vical.domain.SendSocket;
+import com.vical.domain.User;
 import com.vical.rest.IPICServiceRest;
 import com.vical.service.IPersonaService;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
 
 @Component
 public class PICServiceRestImpl implements IPICServiceRest {
 	
 	@Autowired
 	private IPersonaService personaService;
+	@Autowired
+	private Pusher pusherWebsocket;
+	
 
 	@Override
 	public Persona obtenerDatosPersona(String tipoDocumento, String numeroDocumento) throws RuntimeException {
@@ -21,10 +34,60 @@ public class PICServiceRestImpl implements IPICServiceRest {
 
 	@Override
 	public String verificarService(String argumento) {
-		if(StringUtils.isEmpty(argumento)) {
-			throw new RuntimeException();
-		} else {
-			return "Exito";
+		Socket socket = null;
+		try {
+			socket = IO.socket("https://quiet-tundra-41266.herokuapp.com");
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
 		}
+		socket.connect();
+		
+		List<User> usuarios = new ArrayList<>();
+		usuarios.add(new User("Vical", "Rodriguez", "Admin", 12));
+		usuarios.add(new User("Naruto", "Uzumaki", "User", 14));
+		Gson gson = new Gson();
+		SendSocket sendSocket = new SendSocket();
+		sendSocket.setEventName("canal");
+		sendSocket.setBody(gson.toJson(usuarios));
+		socket.emit("job-validator", gson.toJson(sendSocket));
+		
+//		if(StringUtils.isEmpty(argumento)) {
+//			throw new RuntimeException();
+//		} else {
+//			List<User> usuarios = new ArrayList<>();
+//			usuarios.add(new User("Vical", "Rodriguez", "Admin", 12));
+//			usuarios.add(new User("Naruto", "Uzumaki", "User", 14));
+//			Gson gson = new Gson();
+//			pusherWebsocket.trigger("mi-canal", "mi-evento", gson.toJson(usuarios));
+			return "Exito";
+//		}
+	}
+
+	
+	public void verificarService(List<User> users) {
+		
+		Socket socket = null;
+		try {
+			socket = IO.socket("http://localhost:3000");
+		} catch (URISyntaxException e) {
+			System.out.println(e.getMessage());
+		}
+		socket.connect();
+		
+		List<User> usuarios = new ArrayList<>();
+		usuarios.add(new User("Vical", "Rodriguez", "Admin", 12));
+		usuarios.add(new User("Naruto", "Uzumaki", "User", 14));
+		Gson gson = new Gson();
+		socket.emit("evento", gson.toJson(usuarios));
+		
+//		Configuration configuration = new Configuration();
+//		configuration.setHostname("192.168.88.68");
+//		configuration.setPort(4000);
+//		final SocketIOServer server = new SocketIOServer(configuration);
+		
+		//host: 192.168.88.68
+		//port: 4000
+		//pusherWebsocket.trigger("mi-canal", "mi-evento", gson.toJson(users));
+		
 	}
 }
